e013f13e077445150f96d53bf5917d9e03d2ef67
khanh <npkhanh2311@gmail.com>
2015-04-30 22:32:49 +0700

    - add cordova/plugins/GooglePlayGame

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,7 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/com.a42.cordova.googleplaygame/www/google-play-game.js",
+        "id": "com.a42.cordova.googleplaygame.GooglePlayGame",
+        "clobbers": [
+            "googleplaygame"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "com.a42.cordova.googleplaygame": "2.0.4",
+    "com.google.playservices": "21.0.0",
+    "android.support.v4": "21.0.1"
+}
 // BOTTOM OF METADATA
 });
